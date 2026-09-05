a904127a92e0a8087dc2acf68ec3988ff77a4dcf
epic60869-hue <epic60869@gmail.com>
2026-09-05 20:51:32 +0100

    Fix Skysoft ProfitTracker session bridge

--- src/main/java/com/epic60869/tastyfish/mixin/SkysoftProfitTrackerMixin.java
+++ src/main/java/com/epic60869/tastyfish/mixin/SkysoftProfitTrackerMixin.java
@@ -1,15 +1,19 @@
 package com.epic60869.tastyfish.mixin;
 
 import com.epic60869.tastyfish.SkysoftProfitTrackerAccessor;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Final;
 
 import java.util.Map;
 
-@Mixin(targets = "com.skysoft.features.profit.ProfitTracker")
+/**
+ * Bridges Skysoft's internal session tracker to TastyFish without making
+ * Skysoft a hard compile/runtime dependency.
+ */
+@Mixin(targets = "com.skysoft.features.profit.ProfitTracker", remap = false)
 public abstract class SkysoftProfitTrackerMixin implements SkysoftProfitTrackerAccessor {
-    @Shadow @Final private static Map<String, ?> sessionStats;
+    @Shadow @Final private Map<String, ?> sessionStats;
 
     @Override
     public Map<String, ?> tastyfish$getSessionStats() {
